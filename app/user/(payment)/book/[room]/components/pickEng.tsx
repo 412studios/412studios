6ff9dcae88702c8b9b53dee2d3ae912334af6956
Chris Kinsman <chrisknsmn@gmail.com>
2025-03-14 09:05:55 -0400

    Update pickEng.tsx

--- app/user/(payment)/book/[room]/components/pickEng.tsx
+++ app/user/(payment)/book/[room]/components/pickEng.tsx
@@ -10,7 +10,10 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { timeSlots } from "@/app/user/book/components/timeSlots";
+import {
+  timeSlots,
+  subscriptionTimeSlots,
+} from "@/app/user/(payment)/book/components/timeSlots";
 
 export const PickEng = ({
   prices,
