b87704076abbb95f16f7d488093fb06009b19ae5
Chris Kinsman <chrisknsmn@gmail.com>
2025-03-15 11:47:54 -0400

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
     <div className="h-[100vh] w-screen flex justify-center items-center">
       <div className="flex flex-col gap-2">
         <h1>404 - Page Not Found</h1>
-        <p>We couldn't find the page you were looking for.</p>
+        <p>We could not find the page you were looking for.</p>
         <Link href="/">
           <Button>Return to Home</Button>
         </Link>
