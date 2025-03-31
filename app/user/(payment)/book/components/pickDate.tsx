e5d1b4869e40e9893950803c1c4953c6b277f71e
Chris Kinsman <chrisknsmn@gmail.com>
2025-03-31 09:50:33 -0400

    booking

--- app/user/(payment)/book/components/pickDate.tsx
+++ app/user/(payment)/book/components/pickDate.tsx
@@ -1,24 +1,23 @@
 "use client";
-import React, { useEffect } from "react";
+import React from "react";
 import { Calendar } from "@/components/ui/calendar";
 import { useDashboard } from "../context";
 
 export const PickDate = () => {
   const { prices, options, setOptions } = useDashboard();
-  const handleDatePick = (newDate: any) => {
+  const handleDatePick = (newDate: Date) => {
     setOptions({ ...options, date: newDate, startTime: -1, endTime: -1 });
   };
   const [date, setDate] = React.useState<Date | undefined>(new Date());
-  useEffect(() => {
-    setDate(new Date());
-  }, [options.room]);
   return (
     <Calendar
       mode="single"
       selected={date}
       onSelect={(selectedDate) => {
         setDate(selectedDate);
-        handleDatePick(selectedDate);
+        if (selectedDate) {
+          handleDatePick(selectedDate);
+        }
       }}
       className="border rounded-lg border-black hover:bg-black/10 cursor-pointer duration-300"
     />
--- app/user/admin/book/create/components/pickDate.tsx
+++ app/user/admin/book/create/components/pickDate.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect } from "react";
+import React from "react";
 import { Calendar } from "@/components/ui/calendar";
 export const PickDate = ({
   prices,
@@ -10,23 +10,21 @@ export const PickDate = ({
   options: any;
   setOptions: any;
 }) => {
-  const handleDatePick = (newDate: any) => {
+  const handleDatePick = (newDate: Date) => {
     setOptions({ ...options, date: newDate, startTime: -1, endTime: -1 });
   };
   const [date, setDate] = React.useState<Date | undefined>(new Date());
-  useEffect(() => {
-    setDate(new Date());
-  }, [options.room]);
   return (
-    <>
-      <Calendar
-        mode="single"
-        selected={date}
-        onSelect={(selectedDate) => {
-          setDate(selectedDate);
+    <Calendar
+      mode="single"
+      selected={date}
+      onSelect={(selectedDate) => {
+        setDate(selectedDate);
+        if (selectedDate) {
           handleDatePick(selectedDate);
-        }}
-      />
-    </>
+        }
+      }}
+      className="border rounded-lg border-black hover:bg-black/10 cursor-pointer duration-300"
+    />
   );
 };
