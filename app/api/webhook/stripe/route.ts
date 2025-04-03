ae7880becb055a3cb460c0028bf2722525747320
Chris Kinsman <chrisknsmn@gmail.com>
2025-04-03 11:00:27 -0400

    Update route.ts

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -10,9 +10,7 @@ export async function POST(req: Request) {
   const body = await req.text();
   const headersList = await headers();
   const signature = headersList.get("Stripe-Signature") as string;
-
   let event: Stripe.Event;
-
   try {
     event = stripe.webhooks.constructEvent(
       body,
