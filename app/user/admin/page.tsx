4f4aea5d43be7d81fd43ffbfb4af5bef4a2c5e56
chrisknsmn <chrisknsmn@gmail.com>
2025-05-05 12:50:45 -0400

    admin design

--- app/user/admin/page.tsx
+++ app/user/admin/page.tsx
@@ -46,43 +46,44 @@ export default async function Main() {
   const userDetails = await getUserDetails(user?.id as string);
   // deletePendingmembership();
   return (
-    <>
-      <Section>
-        <Card>
-          <CardHeader>
-            <CardTitle>Admin Dashboard</CardTitle>
-          </CardHeader>
-          <div className="px-4">
-            <Link href="/user/admin/pricing">
-              <Button>
-                <span>Update Pricing</span>
-              </Button>
+    <Section>
+      <Card>
+        <CardHeader>
+          <CardTitle>Admin Dashboard</CardTitle>
+        </CardHeader>
+        <div className="px-4">
+          <Link href="/user/admin/pricing">
+            <Button>
+              <span>Update Pricing</span>
+            </Button>
+          </Link>
+        </div>
+        <CardContent className="p-0">
+          <div className="p-4">
+            <Link href="/user/admin/users">
+              <span className="hover:bg-accent hover:text-accent-forground group flex items-center rounded-md px-3 py-2 text-sm font-medium">
+                <Book className="text-primary mr-2 h-4 w-4" />
+                <span>Users</span>
+              </span>
             </Link>
+            <Link href="/user/admin/membership">
+              <span className="hover:bg-accent hover:text-accent-forground group flex items-center rounded-md px-3 py-2 text-sm font-medium">
+                <Book className="text-primary mr-2 h-4 w-4" />
+                <span>Memberships</span>
+              </span>
+            </Link>
+            <Link href="/user/admin/book/">
+              <span className="hover:bg-accent hover:text-accent-forground group flex items-center rounded-md px-3 py-2 text-sm font-medium">
+                <Book className="text-primary mr-2 h-4 w-4" />
+                <span>Bookings</span>
+              </span>
+            </Link>
+            {/* new tab start */}
+            <div className="border rounded-xl py-2 px-3">tabs</div>
+            {/* new tab end */}
           </div>
-          <CardContent className="p-0">
-            <div className="p-4">
-              <Link href="/user/admin/users">
-                <span className="hover:bg-accent hover:text-accent-forground group flex items-center rounded-md px-3 py-2 text-sm font-medium">
-                  <Book className="text-primary mr-2 h-4 w-4" />
-                  <span>Users</span>
-                </span>
-              </Link>
-              <Link href="/user/admin/membership">
-                <span className="hover:bg-accent hover:text-accent-forground group flex items-center rounded-md px-3 py-2 text-sm font-medium">
-                  <Book className="text-primary mr-2 h-4 w-4" />
-                  <span>Memberships</span>
-                </span>
-              </Link>
-              <Link href="/user/admin/book/">
-                <span className="hover:bg-accent hover:text-accent-forground group flex items-center rounded-md px-3 py-2 text-sm font-medium">
-                  <Book className="text-primary mr-2 h-4 w-4" />
-                  <span>Bookings</span>
-                </span>
-              </Link>
-            </div>
-          </CardContent>
-        </Card>
-      </Section>
-    </>
+        </CardContent>
+      </Card>
+    </Section>
   );
 }
